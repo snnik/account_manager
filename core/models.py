694040a96b7a153ed031ab518e92b0bf79088a39
snnik <snnik@live.com>
2019-04-20 07:05:30 +0300

    model change

--- core/models.py
+++ core/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator, EmailValidator
-from django.utils import timezone
+
 
 # Create your models here.
 class Services(models.Model):
-    name = models.CharField(max_length = 20, unique=True)
-    description = models.CharField(max_length = 50)
+    name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=50)
     status = models.BooleanField(default=True)
-    is_create = models.DateTimeField(default=timezone.now())
-    is_update = models.DateTimeField(auto_now= True)
+    is_create = models.DateTimeField(auto_now_add=True)
+    is_update = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
-        #Function logging
+        # Function logging
 
         super(Services, self).save(*args, **kwargs)
 
@@ -20,12 +20,13 @@ class Services(models.Model):
         self.status = False
         self.save()
 
+
 class Contract(models.Model):
-    number = models.CharField(max_length = 10, unique=True)
+    number = models.CharField(max_length=10, unique=True)
     customer = models.ManyToManyField(User)
     status = models.BooleanField(default=True)
     services = models.ManyToManyField(Services)
-    is_create = models.DateTimeField(default=timezone.now())
+    is_create = models.DateTimeField(auto_now_add=True)
     is_update = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
@@ -34,15 +35,14 @@ class Contract(models.Model):
         super(Contract, self).save(*args, **kwargs)
 
 
-
-#Данные о юридичском лице
+# Данные о юридичском лице
 class customer_info(models.Model):
     description = models.CharField(max_length=100)
     OGRN = models.IntegerField(blank=True)
     INN = models.IntegerField(blank=True)
     KPP = models.IntegerField(blank=True)
-    legal_address = models.CharField(max_length= 250)
-    postal_address = models.CharField(max_length= 250)
+    legal_address = models.CharField(max_length=250)
+    postal_address = models.CharField(max_length=250)
     customer = models.OneToOneField(User, on_delete='CASCADE')
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Телефонный номер должен быть иметь следующий формат: '+999999999'. "
@@ -50,7 +50,7 @@ class customer_info(models.Model):
     phone_number = models.CharField(validators=[phone_regex], max_length=17)  # validators should be a list
     email_validator = EmailValidator()
     email_address = models.CharField(validators=[email_validator], max_length=15)
-    is_create = models.DateTimeField(default=timezone.now())
+    is_create = models.DateTimeField(auto_now_add=True)
     is_update = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
@@ -58,11 +58,12 @@ class customer_info(models.Model):
 
         super(customer_info, self).save(*args, **kwargs)
 
-#Log
+
+# Log
 class core_protocol(models.Model):
     action = models.CharField(max_length=200)
     user = models.CharField(max_length=200)
-    action_date = models.DateTimeField(default=timezone.now())
+    action_date = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
         # Function logging
