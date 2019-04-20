7db0c8aaf19304f5b970f7347782617d34fcf7ad
snnik <snnik@live.com>
2019-04-20 19:52:02 +0300

    model change

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Services, Contract
+from .models import *
 
 # Register your models here.
 admin.site.register(Services)
 admin.site.register(Contract)
+admin.site.register(CustomerInfo)
+admin.site.register(CoreProtocol)
