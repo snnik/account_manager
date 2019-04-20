a5a31486e8c9d76c44f14ef6f607c8d2db9a2633
snnik <snnik@live.com>
2019-04-21 00:03:49 +0300

    view-template change

--- core/views.py
+++ core/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 
+
 # Create your views here.
 def index(request):
-    return render(request, "core/base_login.html")
+    print(Services.objects.count())
+    return render(request, "core/account_list.html", context={'services': Services.objects.all()})
 
 
 def delete_account(request):
@@ -13,6 +15,7 @@ def delete_account(request):
 
 def create_account(request):
     return HttpResponse("Create")
+
 
 def update_account(request, **args):
     return HttpResponse(args)
