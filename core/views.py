49a16d0bb94628f24425b241d4d2ce1d96a46249
snnik <snnik@live.com>
2019-04-21 23:45:13 +0300

    view-template change

--- core/views.py
+++ core/views.py
@@ -1,12 +1,19 @@
+from django.contrib import auth
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
 from .models import *
 
 
 # Create your views here.
+@login_required(login_url='base_login')
 def index(request):
+    context = {}
+    context['services'] = Services.objects.all()
+    context['username'] = auth.get_user(request).username
     #print(Services.objects.count())
-    return render(request, "core/account_list.html", context={'services': Services.objects.all()})
+    return render(request, "core/account_list.html", context)
 
 
 def delete_account(request):
@@ -19,3 +26,25 @@ def create_account(request):
 
 def update_account(request, **args):
     return HttpResponse(args)
+
+
+def login(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            login_error = 'User not exist'
+            context = {'login_error': login_error}
+            return render(request, 'core/base_login.html', context)
+    else:
+        return render(request, 'core/base_login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/')
