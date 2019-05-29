b3ce1cc45edb1059e6990289a6f4238880a96ade
snnik <34176697+snnik@users.noreply.github.com>
2019-05-29 09:19:53 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -34,7 +34,7 @@ class ServiceList(ObjectsLists):
     view_title = 'Service'
     page_title = 'Services'
     permission_required = ('core.view_service',)
-    create_uri = 'services_list'
+    create_uri = 'service_create'
 
 
 class PackageList(ObjectsLists):
@@ -43,7 +43,7 @@ class PackageList(ObjectsLists):
     view_title = 'Package'
     page_title = 'Packages'
     permission_required = ('core.view_package',)
-    create_uri = 'package_list'
+    create_uri = 'package_create'
 
 
 class GroupList(ObjectsLists):
